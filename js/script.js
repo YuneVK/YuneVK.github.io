1eb8c80a408d8f7f784989730e89e15df6a6bca4
YuneVK <yune.vk@gmail.com>
2016-08-18 01:50:08 +0200

    Añadir portfolio a la web principal

--- js/script.js
+++ js/script.js
@@ -1,20 +1,46 @@
 $(function() {
 	var height = $(window).height();
 
-	// Cada vez que se haga scroll
-	$(document).scroll(function() {
-		// Obtenemos el valor del scroll
-		var scrollTop = $(this).scrollTop();
-		var px = scrollTop / 70;
+	var device = navigator.userAgent;
 
-		// Si el valor del scroll es menor que el alto de la página continuaremos.
-		// Se establece así para que lo siguiente solo se ejecute cuando la cabecera
-		// esté visible
-		if(scrollTop < height) {
-			// Movemos el fondo del contenedor (la cabecera) a medida
-			// que se va haciendo scroll en la página
-			$("#contenedor").css({"background-position": "center -" + scrollTop / 6 + "px"});
-			$("#logo").css({"top": "calc(-6vh + " + scrollTop + "px/4)"});
-		}
-	});
+/*
+	var widthOrnament = $('#contenidoHeader h1').width();
+	$("#ornament").css({"width": widthOrnament*0.75 + "px"});
+
+	console.log("Ancho: " + widthOrnament);
+*/
+
+	if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i)) {
+		console.log("Movil");
+		$("#estrellas").css({"opacity": "1"});
+
+	} else {
+		/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+		particlesJS.load('particles-js', 'assets/particles.json', function() {
+			console.log('callback - particles.js config loaded');
+		});
+
+		$("#fondo1").css({"background-attachment": "fixed"});
+		$("#fondo2").css({"background-attachment": "fixed"});
+
+		// Cada vez que se haga scroll
+		$(document).scroll(function() {
+			// Obtenemos el valor del scroll
+			var scrollTop = $(this).scrollTop();
+
+			// Si el valor del scroll es menor que el alto de la página continuaremos.
+			// Se establece así para que lo siguiente solo se ejecute cuando la cabecera
+			// esté visible
+			if(scrollTop < height) {
+				// Movemos el fondo del contenedor (la cabecera) a medida
+				// que se va haciendo scroll en la página
+				$("#fondo1").css({"background-position": "center bottom +" + (scrollTop/1.3) + "px"});
+				$("#fondo2").css({"background-position": "center bottom +" + (scrollTop/1.1) + "px"});
+			}
+		});		
+	}
+
+
+
+
 });
