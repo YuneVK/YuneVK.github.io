7c452031ca15f614d578fe818147d89f051ed613
YuneVK <yune.vk@gmail.com>
2016-08-17 19:46:30 +0200

    Modificar

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -6,23 +6,28 @@ $(function() {
 		particlesJS.load('particles-js', 'assets/particles.json', function() {
 			console.log('callback - particles.js config loaded');
 		});
+
+		$("#fondo1").css({"background": "url('../img/fondouno.png') no-repeat fixed"});
+		$("#fondo2").css({"background": "url('../img/fondodos.png') no-repeat fixed"});
+
+		// Cada vez que se haga scroll
+		$(document).scroll(function() {
+			// Obtenemos el valor del scroll
+			var scrollTop = $(this).scrollTop();
+
+			// Si el valor del scroll es menor que el alto de la página continuaremos.
+			// Se establece así para que lo siguiente solo se ejecute cuando la cabecera
+			// esté visible
+			if(scrollTop < height) {
+				// Movemos el fondo del contenedor (la cabecera) a medida
+				// que se va haciendo scroll en la página
+				$("#fondo1").css({"background-position": "center bottom +" + (scrollTop/1.3) + "px"});
+				$("#fondo2").css({"background-position": "center bottom +" + (scrollTop/1.1) + "px"});
+			}
+		});
 	}
 
-	// Cada vez que se haga scroll
-	$(document).scroll(function() {
-		// Obtenemos el valor del scroll
-		var scrollTop = $(this).scrollTop();
 
-		// Si el valor del scroll es menor que el alto de la página continuaremos.
-		// Se establece así para que lo siguiente solo se ejecute cuando la cabecera
-		// esté visible
-		if(scrollTop < height) {
-			// Movemos el fondo del contenedor (la cabecera) a medida
-			// que se va haciendo scroll en la página
-			$("#fondo1").css({"background-position": "center bottom +" + (scrollTop/1.3) + "px"});
-			$("#fondo2").css({"background-position": "center bottom +" + (scrollTop/1.1) + "px"});
-		}
-	});
 
 
 });
