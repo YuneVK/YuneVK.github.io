c1fd932538b9ecbca5c587a052323b86014c9181
YuneVK <yune.vk@gmail.com>
2016-09-29 20:52:40 +0200

    Pruebas

--- pruebas/js/script.js
+++ pruebas/js/script.js
@@ -1,7 +1,15 @@
 $(function() {
 
-	var scene = document.getElementById('scene');
-	var parallax = new Parallax(scene);
+	var $scene = $('#scene').parallax();
+	$scene.parallax('enable');
+	$scene.parallax('disable');
+	$scene.parallax('updateLayers');
+	$scene.parallax('calibrate', false, true);
+	$scene.parallax('invert', false, true);
+	$scene.parallax('limit', false, 10);
+	$scene.parallax('scalar', 1, 2);
+	$scene.parallax('friction', 0.2, 0.8);
+	$scene.parallax('origin', 0.0, 1.0);
 
 
 
