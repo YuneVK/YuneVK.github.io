4eea8be99fbf090ee763cf59c24e8b084d049da4
Sonia Ruiz <yune.vk@gmail.com>
2018-08-01 13:49:50 +0200

    Pruebas Nueva Web

---
+++ nueva-web/js/script.js
@@ -0,0 +1,219 @@
+$(function() {
+    "use strict";
+
+    // Background colors of the diferent sections
+    let backgroundColors = {
+        header: {background: "#EBEBEB", particles: "#FAFAFA"}, 
+        aboutme: {background: "#EBEBEB", particles: "#90A4AE"}, 
+        skills: {background: "#FC6600", particles: "#FFE0B2"}, 
+        timeline: {background: "#003D5B", particles: "#C5CAE9"}, 
+        portfolio: {background: "#EBEBEB", particles: "#E57373"}, 
+        yunemy: {background: "#007A6B", particles: "#B2DFDB"}, 
+        blog: {background: "#EBEBEB", particles: "#81C784"}, 
+        contact: {background: "#2B454E", particles: "#CFD8DC"}
+    };
+
+    let lastElement;
+
+    // Init Particles.js
+    particlesJS.load('particles', 'assets/particlesjs-config.json', function() {
+        console.log('callback - particles.js config loaded');
+        checkBackground();
+    });
+
+    replaceSVG();
+
+
+    // TODO: Window resize event
+    // TODO: Create the nav code. For the scroll events, the first answer on this link might be useful: https://stackoverflow.com/questions/18564187/using-jquery-find-the-div-class-that-the-top-of-the-viewport-is-inside
+
+    $(window).scroll(function() {
+        checkBackground();
+        checkIllustrationPosition();
+    });
+
+    initPortfolio();
+
+    /**
+     * Check the background color/gradient according to the section that is in the viewport
+     */
+    function checkBackground() {
+        let windowTop = Math.max($('body').scrollTop(), $('html').scrollTop());
+
+        // If there are more than one element in the viewport, all of them are saved
+        let visibleElements = [];
+
+        // For each section we will check if they are in the viewport to save or not save it in the array
+        $('.section').each(function (index) {
+            if (windowTop > ($(this).position().top - 150)) {
+                visibleElements.push(this);
+            }
+        });
+
+        // Get the last element visible in the viewport. This element will define the body's background
+        let currentElement = visibleElements.pop();
+
+        // Is the current element is different that the last element
+        if (lastElement != currentElement) {
+            lastElement = currentElement; // Update de last element
+
+            // Change the background color of the body according to the section
+            $('body').css('background', backgroundColors[currentElement.id].background);
+
+            // Change the color of the particles
+            if (typeof pJSDom[0] != 'undefined') {
+                pJSDom[0].pJS.particles.color.value = backgroundColors[currentElement.id].particles;
+                pJSDom[0].pJS.fn.particlesRefresh();
+            }
+            
+            // If the current element is the header and it's not visible, it will be visible
+            if (currentElement.id == $('.section')[0].id) {
+                if ($('.background').hasClass('is-not-visible')) {
+                    $('.background').removeClass('is-not-visible');
+                    $('.background').addClass('is-visible');
+                }
+            } else {
+                // Is the current element is other, and the gradient is visible, it will be not visible
+                if (!$('.background').hasClass('is-not-visible')) {
+                    $('.background').removeClass('is-visible');
+                    $('.background').addClass('is-not-visible');
+                }
+            }
+        }
+    }
+
+    function initPortfolio() {
+        let container = $('.portfolio-gallery');
+
+        $('.navigation li').click(function() {
+            $(this).addClass('active');
+            $('.navigation li').not($(this)).removeClass('active');
+        });
+
+        console.log("InitPortfolio");
+
+        var mixed = mixitup($('.portfolio-gallery .elements'), {
+            animation: {
+                duration: 400,
+                //feffects: 'fade translateZ(-360px) stagger(34ms)',
+                easing: 'ease'
+            },
+            callbacks: {
+                onMixStart: function(state, futureState){
+                    console.log('Animation starting');
+                },onMixEnd: function(state){
+                    console.log('Operation ended');
+                },onMixFail: function(state){
+                    console.log('No elements found matching '+state.activeFilter);
+                },onMixBusy: function(state){
+                    console.log('MixItUp busy');
+                }
+            }
+        });
+
+        $('.portfolio-item', container).click(function() {
+            $('#' + $(this).data('item')).parent().css('display', 'flex');
+            $('body').addClass('modal-open');
+            $('#' + $(this).data('item')).css('display', 'flex').animateCss('fadeIn', function() {
+                
+            });
+
+            // Force the click event to reset the slider
+            $('#' + $(this).data('item') + ' .navigation label:first-child').trigger('click');
+            
+        });
+
+        $('.close-button a').click(function() {
+            $('body').removeClass('modal-open');
+            $(this).parent().parent().animateCss('fadeOut', function() {
+                $('.modal').css('display', 'none');
+                $('.portfolio-modal-container').css('display', 'none');
+            });
+        });
+
+        
+
+        //var mixer = mixitup($('.portfolio-gallery .elements'));
+
+        /*$('.portfolio-item', container).hover(function() {
+            $('.portfolio-item', container).not(this).css('opacity', '.7');
+        }, function() {
+            $('.portfolio-item', container).css('opacity', '1');
+        });*/
+    }
+
+    function checkIllustrationPosition() {
+        let sections = $('section.section .illustration-bg');
+        let pageY = $(window).scrollTop();
+
+        for (let i = 0; i < sections.length; i++) {
+            let element = $(sections[i]);
+            let speed = element.data('speed');
+            let parentSection = element.data('parent-section');
+            let newY = (pageY - $('#' + parentSection).offset().top) * speed / 120;
+
+            element.css('transform', "translate3d(0px, " + newY + "px, 0px)");
+        }
+    }
+
+    function replaceSVG() {
+        jQuery('img.svg').each(function() {
+            var $img = jQuery(this);
+            var imgID = $img.attr('id');
+            var imgClass = $img.attr('class');
+            var imgURL = $img.attr('src');
+
+            jQuery.get(imgURL, function(data) {
+                // Get the SVG tag, ignore the rest
+                var $svg = jQuery(data).find('svg');
+
+                // Add replaced image's ID to the new SVG
+                if (typeof imgID !== 'undefined') {
+                    $svg = $svg.attr('id', imgID);
+                }
+                // Add replaced image's classes to the new SVG
+                if (typeof imgClass !== 'undefined') {
+                    $svg = $svg.attr('class', imgClass + ' replaced-svg');
+                }
+
+                // Remove any invalid XML tags as per http://validator.w3.org
+                $svg = $svg.removeAttr('xmlns:a');
+
+                // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+                if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                    $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+                }
+
+                // Replace image with new SVG
+                $img.replaceWith($svg);
+            }, 'xml');
+        });
+    }
+
+    $.fn.extend({
+        animateCss: function(animationName, callback) {
+            var animationEnd = (function(el) {
+            var animations = {
+                animation: 'animationend',
+                OAnimation: 'oAnimationEnd',
+                MozAnimation: 'mozAnimationEnd',
+                WebkitAnimation: 'webkitAnimationEnd',
+            };
+
+            for (var t in animations) {
+                if (el.style[t] !== undefined) {
+                    return animations[t];
+                }
+            }
+            })(document.createElement('div'));
+
+            this.addClass('animated ' + animationName).one(animationEnd, function() {
+                $(this).removeClass('animated ' + animationName);
+
+                if (typeof callback === 'function') callback();
+            });
+            
+            return this;
+        },
+    });
+});
